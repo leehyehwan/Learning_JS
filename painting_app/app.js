3f3f04ff0ef8ce3e20e90380e2134d089dd5a869
Yvonne Lee <sparrow_hh@naver.com>
2021-01-18 19:07:18 +0900

    start new project(Painting App)

---
+++ painting_app/app.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#00000";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function startpainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function stopPainting(event){
+    painting = false;
+}
+
+
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove)
+    canvas.addEventListener("mousedown", startpainting)
+    canvas.addEventListener("mouseup", stopPainting)
+    canvas.addEventListener("mouseleave", stopPainting)
+}
+
