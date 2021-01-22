0c683dcd35d73e1786726898aeec2022cf523074
Yvonne Lee <sparrow_hh@naver.com>
2021-01-22 20:39:09 +0900

    Done

--- painting_app/app.js
+++ painting_app/app.js
@@ -1,13 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
+const saveBtn = document.getElementById("jsSave")
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_CLOLR = "#000000";
+const CANVAS_SIZE = 500;
 
-ctx.strokeStyle = "#00000";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+ctx.strokeStyle = INITIAL_CLOLR;
+ctx.fillStyle = INITIAL_CLOLR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function startpainting(){
     painting = true;
@@ -30,12 +42,66 @@ function stopPainting(event){
     painting = false;
 }
 
+function changecolor(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
 
+function handelRnageChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill"
+        ctx.fillStyle()
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+    ctx.fillRect(0, 0, canvas.width, canvas.height)}
+}
+
+function handleCM(event){
+    event.preventDefault();
+    window.alert("마우스 우클릭은 사용하실 수 없습니다.");
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "Download_JS";
+    link.click();
+
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove)
     canvas.addEventListener("mousedown", startpainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("click", handleCanvasClick)
+    canvas.addEventListener("contextmenu", handleCM)
 }
 
+Array.from(colors).forEach(color => color.addEventListener("click", changecolor))
+
+if(range){
+    range.addEventListener("input", handelRnageChange)
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick)
+}
