6b898005f5405667476c88fc34f3f786c00e502c
Yvonne Lee <sparrow_hh@naver.com>
2021-02-10 01:18:11 +0900

    slack display setting start

---
+++ slack_display/sheet.js
@@ -0,0 +1,44 @@
+const image = document.querySelector(".img");
+const name = document.querySelector(".header");
+const text = document.querySelector(".text")
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function listMajors() {
+  gapi.client.sheets.spreadsheets.values.get({
+    // spreadsheetId: '1IgZ0Fci_Lw5TSEGbVtVaQ_daaEsDh5nDJ4Fiu-tRQm4',
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    // range: 'kiss!A2:D',
+    range: 'Class Data!A2:E'
+  }).then(function(response) {
+    var range = response.result;
+    console.log(range)
+    // const slackdata = response.result.values
+    if (range.values.length > 0) {
+      appendPre('Name, Major:');
+      for (i = 0; i < range.values.length; i++) {
+        var row = range.values[i];
+        // Print columns A and E, which correspond to indices 0 and 4.
+        appendPre(row[0] + ', ' + row[4]);
+      }
+    } else {
+      appendPre('No data found.');
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+}
