7c1aa5b06aa21c8e367936594c03480634b164dd
Yvonne Lee <sparrow_hh@naver.com>
2021-02-20 20:43:36 +0900

    섭씨 기호 추가

--- momentum_app/weather.js
+++ momentum_app/weather.js
@@ -13,7 +13,7 @@ function getWeather(lat, lon) {
     .then(function (json) {
       const temp = json.main.temp;
       const place = json.name;
-      weather.innerText = `${temp} @ ${place}`;
+      weather.innerText = `${temp}°C @ ${place}`;
     });
 }
 
